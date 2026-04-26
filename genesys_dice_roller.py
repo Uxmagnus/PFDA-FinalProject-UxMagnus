98d8cf3da78eda6be3688f415edb42e304cbc09f
Uxmagnus <72560246+Uxmagnus@users.noreply.github.com>
2026-04-26 14:21:00 -0500

    Option to roll force Dice added. There's no way to configure count in UI just yet.

--- genesys_dice_roller.py
+++ genesys_dice_roller.py
@@ -22,20 +22,29 @@ class dice():
 
 class MyLayout(Widget):
     dice_types = {}
+    # Proficiency Dice Breakdown: Blank, Success, Success, 2 Successes, 2 Successes, Advantage, Success+Advantage, Success+Advantage, Success+Advantage, 2 Advantages, 2 Advantages, Triumph (+Success)
     dice_types["Yellow"] = dice(["NN","SN","SS","AA","SA","SA","AA","SA","SS","SN","AN","SV"])
+    # Ability Dice Breakdown: Blank, Success, Success, 2 Successes, Advantage, Advantage, Success+Advantage, 2 Advantages.
     dice_types["Green"] = dice(["NN","AA","AN","SN","SN","SS","AN","SA"])
+    # Boost Dice Breakdown: Blank, Blank, Success, Success+Advantage, Advantage, Advantage.
     dice_types["Blue"] = dice(["NN","AA","AN","SN","SA","NN"])
+    # Challenge Dice Breakdown: Blank, Failure, Failure, 2 Failures, 2 Failures, Threat, Threat, Failure+Threat, Failure+Threat, 2 Threats, 2 Threats, Despair (+Failure)
     dice_types["Red"] = dice(["FD", "TN", "FN", "TT", "FT", "FF", "FN", "TN", "FF", "TT", "FT", "NN"])
+    # Difficulty Dice Breakdown: Blank, Failure, Failure, 2 Failures, 2 Failures, Threat, Threat, Failure+Threat, Failure+Threat, 2 Threats, 2 Threats, Despair.
     dice_types["Purple"] = dice(["FF", "TN", "FN", "FT", "TN", "TN", "TT", "NN"])
+    # Setback Dice Breakdown: Blank, Blank, Failure, Failure, Threat, Threat.
     dice_types["Black"] = dice(["NN", "FN", "TN", "FN", "TN", "NN"])
-
+    # Force Dice Breakdown: 5 Dark Side, 1 Dark Side * 2, 3 Light Side, 3 Light Side * 2
+    dice_types["Force"] = dice(["BN", "BN", "BN", "BN", "BN", "BB", "LN", "LN","LN", "LL", "LL", "LL"]) 
 
     def press_roll(self, button):
         success = 0
         advantage = 0
         triumph = 0
         despair = 0
-        roll_result = "" 
+        light_points = 0
+        dark_points = 0
+        
         for k,v in MyLayout.dice_types.items():
             for i in range(v.count):
                 result = roll_die(v.faces)
@@ -53,9 +62,14 @@ class MyLayout(Widget):
                             advantage -= 1
                         case "D":
                             despair += 1
+                        case "B":
+                            dark_points += 1
+                        case "L":
+                            light_points += 1
                         case "N":
                             pass
-        self.ids.results_label.text = f"{success} successes, {advantage} advantage, {triumph} triumphs, and {despair} despairs"
+        
+        self.ids.results_label.text = f"{generate_roll_result(success, advantage, triumph, despair, light_points, dark_points)}"
 
     def press_mod_dice(self, dice, mod):
         for k,v in MyLayout.dice_types.items():
@@ -87,6 +101,23 @@ def adjust_value(value, adj, min, max):
     if (return_value < min):
         return_value = min
     return return_value
+
+def generate_roll_result(success, advantage, triumph, despair, light_points, dark_points):
+    roll_result = "" 
+    if success > 0:
+        roll_result += f"successes: {success}\n"
+    elif success < 0:
+        roll_result += f"failures: {success * -1}\n "
+    if advantage > 0:
+        roll_result += f"advantage: {advantage}\n "
+    elif advantage < 0:
+        roll_result += f"threat: {advantage * -1}\n"
+    if triumph > 0:
+        roll_result += f"triumphs: {triumph} "
+    if despair > 0:
+        roll_result += f"despairs: {despair}\n"
+    roll_result += f"Force Points: {light_points} Light / {dark_points} Dark"
+    return roll_result
 
 if __name__ == "__main__":
     DiceRollerApp().run()
